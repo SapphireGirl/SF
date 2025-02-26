ba41f80be5529272f14f6dfa16326fda9812ccbf
SapphireGirl <justinealires@outlook.com>
2025-02-25 19:42:06 -0600

    Added seq logging to front end, built out backend

---
+++ SF.Models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SF.Models
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SF.Server/Extensions/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using SF.Logger;
+
+namespace FizzBuzz.Core.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void RegisterRepos(this IServiceCollection collection)
+    {
+        
+        
+    }
+
+    public static void SetupDbContext(this IServiceCollection collection)
+    {
+       
+    }
+
+    public static void RegisterLogging(this IServiceCollection collection)
+    {
+        collection.AddTransient<ISFLogger, SFLogger>();
+    }
+
+    public static void RegisterAuth(this IServiceCollection collection)
+    {
+        //Register authentication services.
+    }
+}
